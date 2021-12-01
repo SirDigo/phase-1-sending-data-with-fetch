1c3348d327c97a91929a748f695f65b1b1053f81
Caleb Rodriguez <sirdigo6@gmail.com>
2021-12-01 12:57:51 -0500

    commit tree cc180198444eac4c81d3013d1c85d40ba378a893

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name: userName, 
+            email: userEmail,
+        }),
+    })
+    .then(function (res) {
+       return res.json()
+    })
+    .then(function (object) {
+        console.log(document.body.textContent = object.id)
+    })
+    .catch(error => {
+        alert("This is not a drill, you did something wrong");
+        console.log(document.body.textContent = error.message);
+    })
+}
